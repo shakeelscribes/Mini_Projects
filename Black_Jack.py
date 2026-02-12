c48fbe36a18d2c4a126e82a4d6bc809beea02c5a
shakeelscribes <ahamedshakeel2005@gmail.com>
2026-02-12 18:46:22 +0530

    Fix of V2 with Continuous Loop Running the Black Jack

--- Black_Jack.py
+++ Black_Jack.py
@@ -1,45 +1,56 @@
-from art import logo
-print(logo)
+cont = True
+while cont:
+    def play_blackjack():
+        game = input("Do you want play a game of Black Jack ('y' or 'n'): ").lower()
+        if game == "n":
+            return game
 
-import random
+        from art import logo
+        print(logo)
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+        import random
 
-user_card = []
-comp_card = []
+        cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-for i in range(0,2):
-    user_card.append(random.choice(cards))
-    comp_card.append(random.choice(cards))
+        user_card = []
+        comp_card = []
 
-print(f"Your Cards: {user_card}, Current Score: {sum(user_card)}")
-print(f"Computer's First Card: {comp_card[0]}")
-
-exceeds_21 = False
-
-while not exceeds_21:
-
-    another_card = input("Type 'y' to get another Card, Type 'n' to pass: ").lower()
-
-    if another_card == "y":
-        user_card.append(random.choice(cards))
-        if sum(user_card) > 21:
-            print(f"Your Final Hand is {user_card} , final Score: {sum(user_card)}")
-            print("You Went Over, You Lose!😭")
-            exceeds_21 = True
-        else:
-            print(f"Your Cards: {user_card}, Current Score: {sum(user_card)}")
-            print(f"Computer's First Card: {comp_card[0]}")
-
-    elif another_card == "n":
-        com_choice = False
-        while not com_choice:
+        for i in range(0,2):
+            user_card.append(random.choice(cards))
             comp_card.append(random.choice(cards))
-            if sum(comp_card) > 21:
-                print(f"Computer's Final Hand is {comp_card}, Final Score: {sum(comp_card)}")
-                print("You Win!")
-                exceeds_21  = True
-                com_choice = True
-            else:
-                print(f"Your Cards: {user_card}, Current Score: {sum(user_card)}")
-                print(f"Computer's First Card: {comp_card[0]}")
+
+        print(f"Your Cards: {user_card}, Current Score: {sum(user_card)}")
+        print(f"Computer's First Card: {comp_card[0]}")
+
+        exceeds_21 = False
+
+        while not exceeds_21:
+
+            another_card = input("Type 'y' to get another Card, Type 'n' to pass: ").lower()
+
+            if another_card == "y":
+                user_card.append(random.choice(cards))
+                if sum(user_card) > 21:
+                    print(f"Your Final Hand is {user_card} , final Score: {sum(user_card)}")
+                    print("You Went Over, You Lose!😭")
+                    exceeds_21 = True
+                else:
+                    print(f"Your Cards: {user_card}, Current Score: {sum(user_card)}")
+                    print(f"Computer's First Card: {comp_card[0]}")
+
+            elif another_card == "n":
+                com_choice = False
+                while not com_choice:
+                    comp_card.append(random.choice(cards))
+                    if sum(comp_card) > 21:
+                        print(f"Computer's Final Hand is {comp_card}, Final Score: {sum(comp_card)}")
+                        print("You Win!")
+                        exceeds_21  = True
+                        com_choice = True
+                    else:
+                        print(f"Your Cards: {user_card}, Current Score: {sum(user_card)}")
+                        print(f"Computer's First Card: {comp_card[0]}")
+
+    game_cont = play_blackjack()
+    if game_cont == "n":
+        cont = False
